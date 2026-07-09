9e91f1fe50c877b481c9a882eec883073b17df8a
ytuyiuy454878989 <danfwu1@qq.com>
2026-07-09 12:40:55 +0800

    Update packages.sh

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -155,14 +155,16 @@ install_custom_feed() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd openlist2 luci-app-openlist2 luci-app-xunlei
+        msd_lite luci-app-msd_lite cups luci-app-cupsd openlist2 luci-app-openlist2 \
+        luci-app-xunlei linkease luci-app-linkease
 
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
         luci-app-quickstart luci-app-store luci-app-homeproxy luci-app-mosdns
         luci-app-passwall nikki luci-app-nikki mihomo-meta
-        open-app-filter luci-app-oaf lucky luci-app-lucky luci-app-easytier openlist2 luci-app-openlist2 luci-app-xunlei
+        open-app-filter luci-app-oaf lucky luci-app-lucky luci-app-easytier openlist2 luci-app-openlist2
+        luci-app-xunlei linkease luci-app-linkease
         
     )
     local custom_feed_sources=()
